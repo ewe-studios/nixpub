cf088c10f706728ad7456b058f5fa810fde5e462
Alex Ewetumo <trinoxf@gmail.com>
2022-03-07 17:48:02 +0800

    UPT: Ad variable declar

--- modules/k0s/default.nix
+++ modules/k0s/default.nix
@@ -1,36 +1,40 @@
 {
 	pkgs ? import <nixospkg> {}
 }:
-pkgs.stdenv.mkDerivation rec {
-    pname = "k0s";
-    version = "v1.23.3+k0s.1-amd64";
 
-    src = pkgs.fetchurl {
-         url = "https://github.com/k0sproject/k0s/releases/download/v1.23.3%2Bk0s.1/k0s-v1.23.3+k0s.1-amd64";
-         sha256 = "0ki0drb1dsy3izxa6z34qfqms2g4nnmwqxrshz9ih7pqkv2ggl8c";
-    };
+{
+    k0s = pkgs.stdenv.mkDerivation rec {
+      pname = "k0s";
+      version = "v1.23.3+k0s.1-amd64";
 
-    sourceRoot = ".";
-    dontConfigure = true;
-    dontBuild = true;
-    dontUnpack = true;
+      src = pkgs.fetchurl {
+           url = "https://github.com/k0sproject/k0s/releases/download/v1.23.3%2Bk0s.1/k0s-v1.23.3+k0s.1-amd64";
+           sha256 = "0ki0drb1dsy3izxa6z34qfqms2g4nnmwqxrshz9ih7pqkv2ggl8c";
+      };
 
-    buildInputs = [ pkgs.which  ];
+      sourceRoot = ".";
+      dontConfigure = true;
+      dontBuild = true;
+      dontUnpack = true;
 
-    buildPhase = ''
-    '';
+      buildInputs = [ pkgs.which  ];
 
-    installPhase = ''
-         mkdir -p $out/bin;
-         cp $src $out/bin/k0s;
-         chmod +x $out/bin/k0s;
-         patchShebangs $out/bin/k0s;
-    '';
+      buildPhase = ''
+      '';
 
-    meta = {
-         description= "k0s installation module for kubernetes clusters";
-         homepage = "https://docs.k0sproject.io/v1.23.3+k0s.1/";
-         maintainers = with pkgs.maintainers; [ k0sproject influx6 ];
-         platforms = pkgs.lib.platforms.unix;
-    };
+      installPhase = ''
+           mkdir -p $out/bin;
+           cp $src $out/bin/k0s;
+           chmod +x $out/bin/k0s;
+           patchShebangs $out/bin/k0s;
+      '';
+
+      meta = {
+           description= "k0s installation module for kubernetes clusters";
+           homepage = "https://docs.k0sproject.io/v1.23.3+k0s.1/";
+           maintainers = with pkgs.maintainers; [ k0sproject influx6 ];
+           platforms = pkgs.lib.platforms.unix;
+      };
+  };
+
 }
